1f0dfba952155fd8fc89d563ef33b5a54537d230
Lukho-1211 <67160564+Lukho-1211@users.noreply.github.com>
2024-08-28 22:29:28 +0200

    install mongoose then connected

--- server/src/server.js
+++ server/src/server.js
@@ -1,19 +1,28 @@
 const http = require("http");
 const { env } = require("process");
+const mongoose = require('mongoose');
 
 const app = require('./app');
 const {loadPlanetsData} = require('./modules/planets.model')
 
 
 const PORT = process.env.PORT || 8000;
+const MONGO_URL = 'mongodb+srv://NasaForEveryOne:PasswordNasa@nodefirst.nzhuexo.mongodb.net/?retryWrites=true&w=majority&appName=nodefirst';
 
 const server = http.createServer(app);
 
-
+mongoose.connection.once('open', ()=>{
+    console.log('MangoDB connected...');
+});
+mongoose.connection.on('error', (err)=>{
+    console.error(err);
+});
 
 async function startServer(){//when using files/streams its very importend for a file to load all its info before the server starts
     await loadPlanetsData(); //first complete reading before the server starts
-
+   
+    await mongoose.connect(MONGO_URL);
+   
     server.listen(PORT, ()=>{
         console.log(`Listerning to port ${PORT}...`);
     });
