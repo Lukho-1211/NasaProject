475a06e247e12fc044d822706a2126f8b4c0820a
Lukho-1211 <67160564+Lukho-1211@users.noreply.github.com>
2024-09-10 22:19:37 +0200

    entire app fix and mango db connect

--- server/src/services/monogo.js
+++ server/src/services/monogo.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
  
 require('dotenv').config();
- 
-const MONGO_URL = process.env.MONGO_URL;
 
+ //SpamboAlulutho
+const MONGO_URL = process.env.MONGO_URL;
 mongoose.connection.once('open', ()=>{
     console.log('MangoDB connected...');
 });
